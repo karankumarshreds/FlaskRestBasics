052edb99429ba3ec33e1b065e87466e4542bb60e
karankumarshreds <naileditnerd@gmail.com>
2021-02-17 00:54:42 +0530

    error handler

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 # initialize app as an API
@@ -11,22 +11,29 @@ names = {
   }
 }
 
-name_post_args = reqparse.RequestParser()
+ages = [ 25, 26]
+
 # body validation
-name_post_args.add_argument("age", type=int, help="Need to send age")
+name_post_args = reqparse.RequestParser()
+name_post_args.add_argument("age", type=int, help="Need to send age", required=True)
+
+
+def abort_if_age_not_exist(age): 
+  if age not in ages:
+    abort(404, message="Age is not valid")
+
 
 # create a resource 
 class HelloWorld(Resource):
   def get(self, name):
-    return { "details": names[name] }
+    return { "details": names[name] }, 202
   def post(self, name): 
     args = name_post_args.parse_args()
-    return { "you sent": args.age }
+    abort_if_age_not_exist(args.age)
+    return { "User Age": "VALID" }
 
 # register it as a resource 
 api.add_resource(HelloWorld, "/people/<string:name>")
 
 if __name__ == "__main__":
   app.run(debug=True)
-
-
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://localhost:5000"
 
-response = requests.post(BASE + "/people/karan", { "age": 26 })
+response = requests.post(BASE + "/people/karan", { "age": 29 })
 print(response.json())

af0158d843ffa0214f43e52f0cdb041cda9814ea
karankumarshreds <naileditnerd@gmail.com>
2021-02-17 00:28:26 +0530

    req body validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request 
-from flask_restful import Api, Resource
+from flask import Flask 
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 # initialize app as an API
@@ -11,14 +11,17 @@ names = {
   }
 }
 
+name_post_args = reqparse.RequestParser()
+# body validation
+name_post_args.add_argument("age", type=int, help="Need to send age")
+
 # create a resource 
 class HelloWorld(Resource):
   def get(self, name):
     return { "details": names[name] }
   def post(self, name): 
-    # the body comes in as a form 
-    print(request.form)
-    return { "method": "Hey there" }
+    args = name_post_args.parse_args()
+    return { "you sent": args.age }
 
 # register it as a resource 
 api.add_resource(HelloWorld, "/people/<string:name>")
--- test.py
+++ test.py
@@ -4,6 +4,3 @@ BASE = "http://localhost:5000"
 
 response = requests.post(BASE + "/people/karan", { "age": 26 })
 print(response.json())
-
-
-
